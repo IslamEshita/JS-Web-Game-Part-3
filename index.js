6823b3f2d09ac44eac0e56bfbae763cbeb0c0a49
Eshita Islam <islam.eshita@gmail.com>
2023-01-27 21:01:37 -0800

    Did step 2

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-function newImage(url, left, bottom){
-    let image = document.createElement('img')
-    image.src = url
+function move(image, left, bottom) {
     image.style.position = 'fixed'
     image.style.left = left + 'px'
     image.style.bottom = bottom + 'px'
+}
+
+
+function newImage(url){
+    let image = document.createElement('img')
+    image.src = url
     document.body.append(image)
     return image
 }
 
-newImage('assets/green-character.gif', 100, 250)
-newImage('assets/tree.png', 200, 450)
-newImage('assets/pillar.png', 350, 250)
-newImage('assets/pine-tree.png', 450, 350)
-newImage('assets/crate.png', 150, 350)
-newImage('assets/well.png', 500, 575)
+let greenCharacter = newImage('assets/green-character.gif')
+move(greenCharacter, 100, 250)
+
+// newImage('assets/tree.png', 200, 450)
+// newImage('assets/pillar.png', 350, 250)
+// newImage('assets/pine-tree.png', 450, 350)
+// newImage('assets/crate.png', 150, 350)
+// newImage('assets/well.png', 500, 575)
 
 
 function newItem(url, left, bottom){
@@ -27,9 +33,9 @@ function newItem(url, left, bottom){
     return item
 }
 
-newItem('assets/sword.png', 500, 555)
-newItem('assets/shield.png', 165, 335)
-newItem('assets/staff.png', 600, 250)
+// newItem('assets/sword.png', 500, 555)
+// newItem('assets/shield.png', 165, 335)
+// newItem('assets/staff.png', 600, 250)
 
 function newInventory(){
     let inventory = document.createElement('div')
